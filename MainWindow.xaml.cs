10bf4cea91aa0e73db27e217f2286c4dadc919dc
gaozhanli <2265705697@qq.com>
2019-11-29 21:11:00 +0800

    科学计算器布局

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -32,7 +32,7 @@ namespace Calculator
             TempResult = this.Result;
             TempInut = this.Input;
         }
-        //符号收集 "x³" "√x" "y√x" "1/x" "10"
+
         //当选项卡发生变化时
         private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -45,10 +45,6 @@ namespace Calculator
             {
                 this.Result = this.Result1;
                 this.Input = this.Input1;
-            }
-            if(tabControl.SelectedIndex == 2)  //界面三，即程序员计算器
-            {
-
             }
         }
 
@@ -118,7 +114,7 @@ namespace Calculator
         //数字
         private void Number_Button(object sender, RoutedEventArgs e)
         {
-            if (this.Input.Text.Contains("("))
+            if (this.Input.Text.Contains("!"))
                 return;
             var button = sender as Button;
             this.Input.Text = this.Input.Text + button.Content;
@@ -132,6 +128,8 @@ namespace Calculator
                 return;
 
             string operation = "";   //操作符
+            bool isSquare = false;  //判断是否为开方或次方运算
+            string index = "";         //保存指数
             var button = sender as Button;
             switch (button.Content)
             {
@@ -146,6 +144,60 @@ namespace Calculator
                 case "÷":
                     operation = "/";
                     break;
+                case "x²":
+                    isSquare = true;
+                    index = "2";
+                    break;
+                case "x³":
+                    isSquare = true;
+                    index = "3";
+                    break;
+                case "√x":
+                    isSquare = true;
+                    index = "0.5";
+                    break;
+                case "1/x":
+                    isSquare = true;
+                    index = "-1";
+                    break;
+                case "xʸ":
+                    if (num != "")
+                    {
+                        arrayList.Add(num);
+                        this.Result.Text = this.Result.Text + num;
+                    }
+                    arrayList.Add("^");
+                    arrayList.Add("(");
+                    this.Result.Text = this.Result.Text + "^" + "(";
+                    this.Input.Text = "";
+                    return;
+                case "y√x":
+                    if (num != "")
+                    {
+                        arrayList.Add(num);
+                        this.Result.Text = this.Result.Text + num;
+                    }
+                    arrayList.Add("^");
+                    arrayList.Add("(");
+                    arrayList.Add("1");
+                    arrayList.Add("/");
+                    this.Result.Text = this.Result.Text + "^(1/";
+                    this.Input.Text = "";
+                    return;
+            }
+
+            if (isSquare)  //为开方或者次方运算
+            {
+                if (num != "")
+                {
+                    this.Result.Text = this.Result.Text + num;
+                    arrayList.Add(num);
+                    this.Input.Text = "";
+                }
+                arrayList.Add("^");
+                arrayList.Add(index);
+                this.Result.Text = this.Result.Text + "^" + index;
+                return;
             }
 
             if (this.Input.Text == "")  //如果此时为空，表示已经点击过运算符，此时进行替换
@@ -161,7 +213,7 @@ namespace Calculator
                         this.Result.Text += arrayList[i];
                     return;
                 }
-                else  //删除数据，数组中最后一元素为数字的情况
+                else  //删除数据后，数组中最后一元素为数字的情况
                 {
                     this.Result.Text = this.Result.Text + operation;
                     arrayList.Add(operation);
@@ -173,6 +225,12 @@ namespace Calculator
             if (this.Result.Text.Contains("="))
                 this.Result.Text = "";
             this.Result.Text = this.Result.Text + num + operation;
+            if (isN)
+            {
+                isN = false;  //阶乘阶段已经添加过数据
+                arrayList.Add(operation);
+                return;
+            }
             arrayList.Add(num);
             arrayList.Add(operation);
         }
@@ -195,15 +253,121 @@ namespace Calculator
         //等于
         private void Equal_Button(object sender, RoutedEventArgs e)
         {
+            this.Result.Text = "";
+            for (int i = 0; i < arrayList.Count; i++)
+                this.Result.Text += arrayList[i];
+            return;
             string num = this.Input.Text;
-            if (num == "")
-                return;
             arrayList.Add(num);
             Logical logical = new Logical(); 
             string result = logical.Analysis(arrayList);  //引用dll文件计算方法
             this.Result.Text = this.Result.Text + num + "=" + result;
             this.Input.Text = result;
             arrayList.Clear();
+        }
+
+        //转换模式
+        private bool isShift = false;
+        private void Shift_Button(object sender, RoutedEventArgs e)
+        {
+            isShift = isShift == false ? true : false;
+            if (isShift)  //按键名字变换
+            {
+                Square.Content = "x³";
+                MulSquare.Content = "y√x";
+                Sin.Content = "sin-1";
+                Cos.Content = "cos-1";
+                Tan.Content = "tan-1";
+                Radical.Content = "1/x";
+                NumSquare.Content = "e^x";
+            }
+            else
+            {
+                Square.Content = "x²";
+                MulSquare.Content = "xʸ";
+                Sin.Content = "sin";
+                Cos.Content = "cos";
+                Tan.Content = "tan";
+                Radical.Content = "√x";
+                NumSquare.Content = "10^x";
+            }
+        }
+
+        //左括号、右括号
+        private void Bracket_Button(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            string value = (string)button.Content;
+            if(value == "(")
+            {
+                if (this.Input.Text != "")
+                    return;
+            }
+            else
+            {
+                string num = this.Input.Text;
+                if (num != "")
+                    arrayList.Add(num);
+                this.Result.Text = this.Result.Text + num;
+                this.Input.Text = "";
+            }
+            this.Result.Text = this.Result.Text + value;
+            arrayList.Add(value);
+        }
+
+        //圆周率和e
+        private void Pi_Button(object sender, RoutedEventArgs e)
+        {
+            if (this.Input.Text != "")
+                return;
+            var button = sender as Button;
+            this.Input.Text = (string)button.Content;
+        }
+
+        //阶乘
+        bool isN = false;
+        private void N_Button(object sender, RoutedEventArgs e)
+        {
+            string num = this.Input.Text;  //进行阶乘运算的数字
+            if (num == "")
+                return;
+            isN = true;
+            this.Input.Text = this.Input.Text + " !";
+            arrayList.Add(num);
+            arrayList.Add("!");
+        }
+
+        //三角函数
+        private void Triangle_Button(object sender, RoutedEventArgs e)
+        {
+            if (this.Input.Text != "")
+                return;
+            var button = sender as Button;
+            arrayList.Add((string)button.Content);
+            arrayList.Add("(");
+            this.Result.Text = this.Result.Text + (string)button.Content + "(";
+        }
+
+        //固定数的次方
+        private void MulSquare_Button(object sender, RoutedEventArgs e)
+        {
+            if (this.Input.Text != "")
+                return;
+            var button = sender as Button;
+            if ((string)button.Content == "e^x")
+            {
+                arrayList.Add("e");
+                arrayList.Add("^");
+                arrayList.Add("(");
+                this.Result.Text = this.Result.Text + "e^(";
+            }
+            else if ((string)button.Content == "10^x")
+            {
+                arrayList.Add("10");
+                arrayList.Add("^");
+                arrayList.Add("(");
+                this.Result.Text = this.Result.Text + "10^(";
+            }
         }
 
         //判断是否为运算符
